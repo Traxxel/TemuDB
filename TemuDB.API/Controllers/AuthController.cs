8db49e80d7511df02db790bf82acc9bb84a82d29
Traxxel <githubcopilot@eisenburgerchase.com>
2025-08-31 13:07:19 +0200

    Fehler behoben: API-Endpunkt für 'Alle Benutzer' hinzugefügt

--- TemuDB.API/Controllers/AuthController.cs
+++ TemuDB.API/Controllers/AuthController.cs
@@ -9,17 +9,17 @@ namespace TemuDB.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly AuthService _authService;
-        
+
         public AuthController(AuthService authService)
         {
             _authService = authService;
         }
-        
+
         [HttpPost("login")]
         public IActionResult Login([FromBody] LoginRequest request)
         {
             var user = _authService.AuthenticateUser(request.Username, request.Password);
-            
+
             if (user != null)
             {
                 return Ok(new
@@ -34,10 +34,10 @@ namespace TemuDB.API.Controllers
                     }
                 });
             }
-            
+
             return Unauthorized(new { success = false, message = "Ungültige Anmeldedaten" });
         }
-        
+
         [HttpPost("register")]
         public IActionResult Register([FromBody] RegisterRequest request)
         {
@@ -45,34 +45,41 @@ namespace TemuDB.API.Controllers
             {
                 return BadRequest(ModelState);
             }
-            
+
             var success = _authService.RegisterUser(request);
-            
+
             if (success)
             {
                 return Ok(new { success = true, message = "Registrierung erfolgreich. Bitte warten Sie auf die Freischaltung durch den Administrator." });
             }
-            
+
             return BadRequest(new { success = false, message = "Benutzername bereits vergeben" });
         }
-        
+
         [HttpGet("users/inactive")]
         public IActionResult GetInactiveUsers()
         {
             var inactiveUsers = _authService.GetInactiveUsers();
             return Ok(inactiveUsers);
         }
-        
+
+        [HttpGet("users/all")]
+        public IActionResult GetAllUsers()
+        {
+            var allUsers = _authService.GetAllUsers();
+            return Ok(allUsers);
+        }
+
         [HttpPost("users/{userId}/activate")]
         public IActionResult ActivateUser(int userId)
         {
             var success = _authService.ActivateUser(userId);
-            
+
             if (success)
             {
                 return Ok(new { success = true, message = "Benutzer erfolgreich aktiviert" });
             }
-            
+
             return NotFound(new { success = false, message = "Benutzer nicht gefunden" });
         }
     }
